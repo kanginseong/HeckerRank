07852e2a3647e9c3baf920fe1682d52d6e235bf9
kanginseong <60174227+kanginseong@users.noreply.github.com>
2021-04-13 22:48:37 +0900

    Update Java If-Else.java

--- Java If-Else.java
+++ Java If-Else.java
@@ -8,14 +8,11 @@ import java.util.regex.*;
 
 public class Solution {
 
-
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         
         if(N % 2 == 1 || ((N % 2 == 0) && (N >= 6 && N <= 20))) {
             System.out.println("Weird");
@@ -23,7 +20,6 @@ public class Solution {
         else {
             System.out.println("Not Weird");
         }
-        
         
         scanner.close();
     }
